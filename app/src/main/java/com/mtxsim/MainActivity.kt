6143dd8e275edb30d3b7305748c3b944f87e5e56
kamil-markiewicz <kamil.markiewicz@mycit.ie>
2019-07-25 22:16:26 +0100

    Implemented rudimentary vp purchase dialog

--- app/src/main/java/com/mtxsim/MainActivity.kt
+++ app/src/main/java/com/mtxsim/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.mtxsim
 
+import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import android.content.DialogInterface
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,16 +16,50 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickBuyVP(view: View){
-        val counterText = findViewById<TextView>(R.id.textViewVPCount)
-        counterText.text = (counterText.text.toString().toInt() + 1).toString()
-        Toast.makeText(this@MainActivity, "You purchased 1 point!", Toast.LENGTH_SHORT).show()
+        //val counterText = findViewById<TextView>(R.id.textViewVPCount)
+        //counterText.text = (counterText.text.toString().toInt() + 1).toString()
+        //val pValues1 = PurchaseValues(2.0, 2)
+        //val pValues2 = PurchaseValues(5.0, 6)
+        //val pValues3 = PurchaseValues(10.0, 13)
+        //val pValues4 = PurchaseValues(20.0, 27)
+        //val pValues5 = PurchaseValues(50.0, 60)
+        //Toast.makeText(this@MainActivity, getVPValueString(pValues2), Toast.LENGTH_SHORT).show()
+
+        buyVPDialog()
     }
 
     fun onClickBuyItem(view: View){
-        val counterVPText = findViewById<TextView>(R.id.textViewVPCount)
-        counterVPText.text = (counterVPText.text.toString().toInt() - 2).toString()
+        adjustVP(-2)
         val counterItemText = findViewById<TextView>(R.id.textViewItemsCount)
         counterItemText.text = (counterItemText.text.toString().toInt() + 1).toString()
         Toast.makeText(this@MainActivity, "You purchased an item for 2 points!", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun buyVPDialog(){
+        val dialogBuilder = AlertDialog.Builder(this)
+        dialogBuilder.setMessage(getString(R.string.ChooseVpAmount))
+            .setPositiveButton("Buy", DialogInterface.OnClickListener {
+                    dialog, id -> dialog.dismiss()
+                    adjustVP(2)
+                    Toast.makeText(this@MainActivity, "You purchased 2 VP!", Toast.LENGTH_SHORT).show()
+            })
+            .setNegativeButton("Cancel", DialogInterface.OnClickListener {
+                    dialog, id -> dialog.cancel()
+            })
+
+        val alert = dialogBuilder.create()
+        alert.setTitle("VP Purchase")
+        alert.show()
+    }
+
+    private fun adjustVP(diff: Int){
+        val counterText = findViewById<TextView>(R.id.textViewVPCount)
+        counterText.text = (counterText.text.toString().toInt() + diff).toString()
+    }
+
+    fun getVPValueString(pv: PurchaseValues): String {
+        val value = pv.value
+        val vp = pv.vpGiven
+        return "$value" + " Euro for " + "$vp" + "VP"
     }
 }
---
+++ app/src/main/java/com/mtxsim/PurchaseValues.kt
@@ -0,0 +1,3 @@
+package com.mtxsim
+
+data class PurchaseValues(val value: Double = 0.0, val vpGiven: Int = 0)
